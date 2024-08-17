e3f64cd03394915d88a813eb435ef51387ef9ec3
XANX1998 <133981437+XANX1998@users.noreply.github.com>
2024-08-17 20:19:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PricingCard from './components/PricingCard';
+
+function App() {
+    return (
+        <div className="App">
+            <PricingCard />
+        </div>
+    );
+}
+
+export default App;
